8b61389ae7a775f78802b291e4649d530eea88f9
Ninjeong <nathan508snap@gmail.com>
2026-06-08 18:20:40 -0400

    Complete 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removal) {
+    return arr.filter(element => !removal.includes(element));
 };
 
 // Do not edit below this line
