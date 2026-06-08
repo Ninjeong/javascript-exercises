4356f4464691ea37b9398a0bc446314caa43c244
Ninjeong <nathan508snap@gmail.com>
2026-06-08 14:03:40 -0400

    Complete 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let str = word.split("");
+    str = str.reverse();
+    return str.join("");
 };
 
 // Do not edit below this line
