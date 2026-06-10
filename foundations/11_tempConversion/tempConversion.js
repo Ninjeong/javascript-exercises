39720d4a00bd47f66504933d7efed806df885abe
Ninjeong <nathan508snap@gmail.com>
2026-06-10 16:15:37 -0400

    Complete 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+    let multiplier = Math.pow(10, 1 || 0);
+    return Math.round((fTemp - 32) * (5/9) * multiplier) / multiplier;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+    let multiplier = Math.pow(10, 1 || 0);
+    return Math.round((cTemp * (9/5) + 32) * multiplier) / multiplier;
 };
 
 // Do not edit below this line
