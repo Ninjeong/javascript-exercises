9412c26ade0d7007e042c40af48673eadbc06a1d
Ninjeong <nathan508snap@gmail.com>
2026-06-10 13:20:36 -0400

    Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || Number.isInteger(num1) === false || Number.isInteger(num2) === false) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (num1 < num2) {
+        sum = num1;
+        for (let i = num1 + 1; i <= num2; i++) {
+            sum += i;
+        }
+    }
+    else {
+        sum = num2;
+        for (let i = num2 + 1; i <= num1; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
