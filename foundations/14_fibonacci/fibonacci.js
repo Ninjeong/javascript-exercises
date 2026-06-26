c26d2252e13e3d434b507758a78a361640d4bfc6
Ninjeong <nathan508snap@gmail.com>
2026-06-26 17:14:28 -0400

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(target) {
 
+    if(Number(target) === 0) return 0;
+    if(Number(target) < 0) return "OOPS";
+    
+    let sum1 = 0;
+    let sum2 = 1;
+    let sum3 = 1;
+    for (let i = 1; i < Number(target); i++) {
+
+        sum3 = sum1 + sum2;
+        sum1 = sum2;
+        sum2 = sum3;
+    }
+    return sum3;
 };
 
 // Do not edit below this line
