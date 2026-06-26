9cbd590048e2461d35cf3e7f5d8ec08314ee5124
Ninjeong <nathan508snap@gmail.com>
2026-06-26 16:46:17 -0400

    Complete 13_palindrome

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = str.toLowerCase();
+    let newNewStr = newStr.replace(/[^a-zA-Z0-9]/g, "");
+    let reversed = "";
 
+    for (let i = newNewStr.length - 1; i >= 0; i--) {
+        reversed += newNewStr[i];
+    }
+
+    return reversed === newNewStr;
 };
 
 // Do not edit below this line
