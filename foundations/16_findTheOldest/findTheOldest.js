e701e28bc6865aba0adf578a149ed720f5b6e69a
Ninjeong <nathan508snap@gmail.com>
2026-06-29 11:52:25 -0400

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const getAge = (person) => {
+    const death = person.yearOfDeath || new Date().getFullYear();
+    return death - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        return getAge(current) > getAge(oldest) ? current : oldest;
+    });
 };
 
 // Do not edit below this line
